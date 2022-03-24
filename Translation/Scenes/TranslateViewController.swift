e971cd3b16db9d116b14c71435fdec5fa287ab3d
TDCIAN <tdcian71@gmail.com>
2022-03-25 00:04:55 +0900

    config translate view

--- Translation/Scenes/TranslateViewController.swift
+++ Translation/Scenes/TranslateViewController.swift
@@ -53,6 +53,33 @@ final class TranslateViewController: UIViewController {
         return label
     }()
     
+    private lazy var bookmarkButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "bookmark"), for: .normal)
+        return button
+    }()
+    
+    private lazy var copyButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "doc.on.doc"), for: .normal)
+        return button
+    }()
+    
+    private lazy var sourceLabelBaseButton: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBackground
+        return view
+    }()
+    
+    private lazy var sourceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "텍스트 입력"
+        label.textColor = .tertiaryLabel
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 23.0, weight: .semibold)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .secondarySystemBackground
@@ -62,13 +89,59 @@ final class TranslateViewController: UIViewController {
 
 private extension TranslateViewController {
     func setupViews() {
-        [buttonStackView]
+        [
+            buttonStackView,
+            resultBaseView,
+            resultLabel,
+            bookmarkButton,
+            copyButton,
+            sourceLabelBaseButton,
+            sourceLabel
+        ]
             .forEach { view.addSubview($0) }
+        
+        let defaultSpacing: CGFloat = 16.0
         
         buttonStackView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.leading.trailing.equalToSuperview().inset(16.0)
+            $0.leading.trailing.equalToSuperview().inset(defaultSpacing)
             $0.height.equalTo(50.0)
+        }
+        
+        resultBaseView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(buttonStackView.snp.bottom).offset(defaultSpacing)
+            $0.bottom.equalTo(bookmarkButton.snp.bottom).offset(defaultSpacing)
+        }
+        
+        resultLabel.snp.makeConstraints {
+            $0.leading.equalTo(resultBaseView.snp.leading).inset(24.0)
+            $0.trailing.equalTo(resultBaseView.snp.trailing).inset(24.0)
+            $0.top.equalTo(resultBaseView.snp.top).inset(24.0)
+        }
+        
+        bookmarkButton.snp.makeConstraints {
+            $0.leading.equalTo(resultLabel.snp.leading)
+            $0.top.equalTo(resultLabel.snp.bottom).offset(24.0)
+            $0.width.height.equalTo(40.0)
+        }
+        
+        copyButton.snp.makeConstraints {
+            $0.leading.equalTo(bookmarkButton.snp.trailing).offset(8.0)
+            $0.top.equalTo(bookmarkButton.snp.top)
+            $0.width.height.equalTo(40.0)
+        }
+        
+        sourceLabelBaseButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(resultBaseView.snp.bottom).offset(defaultSpacing)
+            $0.bottom.equalToSuperview().inset(tabBarController?.tabBar.frame.height ?? 0.0)
+        }
+        
+        sourceLabel.snp.makeConstraints {
+            $0.leading.equalTo(sourceLabelBaseButton.snp.leading).inset(24.0)
+            $0.trailing.equalTo(sourceLabelBaseButton.snp.trailing).inset(24.0)
+            $0.top.equalTo(sourceLabelBaseButton.snp.top).inset(24.0)
         }
     }
 }
